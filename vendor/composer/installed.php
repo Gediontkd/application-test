168e497ca0bb9e442eb12ab7fd95de27d1e4bca4
Gedion Daniel <gediondaniel321@gmail.com>
2024-06-25 09:35:56 +0200

    updated files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '790a0563f92daf5c5e98783941d0017188279487',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '790a0563f92daf5c5e98783941d0017188279487',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webonyx/graphql-php' => array(
+            'pretty_version' => 'v15.12.5',
+            'version' => '15.12.5.0',
+            'reference' => '7bcd31d1dcf67781ed5cb493b22c519c539c05e6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webonyx/graphql-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
